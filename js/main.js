d4250ca7ad4a2b47feef21423c1566273d7f9e43
CarlosBetanco <102324581+CarlosBetanco@users.noreply.github.com>
2023-08-10 18:51:25 -0500

    Se agrego el proyecto

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const showMenu = (headerToggle,navbarId)=>{
+    const toggleBtn  = getElementById(headerToggle);
+    nav = document.getElementById(navbarId)
+
+if (headerToggle && navbarId){
+    toggleBtn.addEventListener('click' , () => {
+        nav.classList.toggle('show-menu')
+        toggleBtn.classList.toggle('fa-times')
+    })
+  }
+
+}
+showMenu('header-toggle', 'navbar')
+
+const linkcolor = document.querySelectorAll('.nav_link');
+
+function colorLink(){
+linkcolor.forEach(1 => 1.classList.remove('active'))
+this.classList.add('active')
+}
+linkcolor.forEach(1 => 1.addEventListener('click',colorLink))
